c62289052a1993f52df54190d844b0668988fcc8
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2021-03-21 19:51:06 +0200

    Fix documentation

--- lib/binance_futures/rest/market.ex
+++ lib/binance_futures/rest/market.ex
@@ -37,6 +37,39 @@ defmodule BinanceFutures.Rest.MarketData do
 
   @doc """
   Gets current exchange trading rules and symbol information.
+
+  ## Example
+
+      iex(1)> BinanceFutures.Rest.MarketData.exchange_info
+      {:ok,
+        %{
+          "exchangeFilters" => [],
+          "futuresType" => "U_MARGINED",
+          "rateLimits" => [
+            %{
+              "interval" => "MINUTE",
+              "intervalNum" => 1,
+              "limit" => 2400,
+              "rateLimitType" => "REQUEST_WEIGHT"
+            },
+            %{
+              "interval" => "MINUTE",
+              "intervalNum" => 1,
+              "limit" => 1200,
+              "rateLimitType" => "ORDERS"
+            },
+            %{
+              "interval" => "SECOND",
+              "intervalNum" => 10,
+              "limit" => 300,
+              "rateLimitType" => "ORDERS"
+            }
+          ],
+          "serverTime" => 1616348890107,
+          ...
+          "timezone" => "UTC"
+      }}
+
   """
   @spec exchange_info() :: {:ok, map} | HTTPClient.error()
   def exchange_info(),
@@ -241,14 +274,14 @@ defmodule BinanceFutures.Rest.MarketData do
 
   ## Example
 
-    iex(1)> BinanceFutures.Rest.MarketData.continuous_klines("BTCUSDT", "PERPETUAL", "5m", nil, nil, 2)
-    {:ok,
-    [
-      [1616340000000, "57480.91", "57490.00", "57372.26", "57405.05", "571.101",
-        1616340299999, "32796220.87635", 9024, "241.532", "13870331.23961", "0"],
-      [1616340300000, "57405.05", "57430.00", "57350.00", "57385.99", "458.456",
-        1616340599999, "26307719.07498", 6574, "198.989", "11419479.03403", "0"]
-    ]}
+      iex(1)> BinanceFutures.Rest.MarketData.continuous_klines("BTCUSDT", "PERPETUAL", "5m", nil, nil, 2)
+      {:ok,
+      [
+        [1616340000000, "57480.91", "57490.00", "57372.26", "57405.05", "571.101",
+          1616340299999, "32796220.87635", 9024, "241.532", "13870331.23961", "0"],
+        [1616340300000, "57405.05", "57430.00", "57350.00", "57385.99", "458.456",
+          1616340599999, "26307719.07498", 6574, "198.989", "11419479.03403", "0"]
+      ]}
 
   """
   @spec continuous_klines(
@@ -285,14 +318,14 @@ defmodule BinanceFutures.Rest.MarketData do
 
   ## Example
 
-    iex(1)> BinanceFutures.Rest.MarketData.index_price_klines("BTCUSDT", "5m", nil, nil, 2)
-    {:ok,
-    [
-      [1616340600000, "57366.98431599", "57429.73139441", "57284.92735200",
-        "57284.92735200", "0", 1616340899999, "0", 300, "0", "0", "0"],
-      [1616340900000, "57283.26395200", "57294.43035200", "57224.03776400",
-        "57265.85639717", "0", 1616341199999, "0", 89, "0", "0", "0"]
-    ]}
+      iex(1)> BinanceFutures.Rest.MarketData.index_price_klines("BTCUSDT", "5m", nil, nil, 2)
+      {:ok,
+      [
+        [1616340600000, "57366.98431599", "57429.73139441", "57284.92735200",
+          "57284.92735200", "0", 1616340899999, "0", 300, "0", "0", "0"],
+        [1616340900000, "57283.26395200", "57294.43035200", "57224.03776400",
+          "57265.85639717", "0", 1616341199999, "0", 89, "0", "0", "0"]
+      ]}
 
   """
   @spec index_price_klines(
@@ -320,14 +353,14 @@ defmodule BinanceFutures.Rest.MarketData do
 
   ## Example
 
-    iex(1)> BinanceFutures.Rest.MarketData.mark_price_klines("BTCUSDT", "5m", nil, nil, 2)
-    {:ok,
-    [
-      [1616340900000, "57302", "57319.11709042", "57185.40000000",
-        "57247.46000000", "0", 1616341199999, "0", 300, "0", "0", "0"],
-      [1616341200000, "57240", "57311.07657677", "57240", "57307.87695491", "0",
-        1616341499999, "0", 53, "0", "0", "0"]
-    ]}
+      iex(1)> BinanceFutures.Rest.MarketData.mark_price_klines("BTCUSDT", "5m", nil, nil, 2)
+      {:ok,
+      [
+        [1616340900000, "57302", "57319.11709042", "57185.40000000",
+          "57247.46000000", "0", 1616341199999, "0", 300, "0", "0", "0"],
+        [1616341200000, "57240", "57311.07657677", "57240", "57307.87695491", "0",
+          1616341499999, "0", 53, "0", "0", "0"]
+      ]}
 
   """
   @spec mark_price_klines(
@@ -355,48 +388,46 @@ defmodule BinanceFutures.Rest.MarketData do
 
   ## Example
 
-    iex(1)> BinanceFutures.Rest.MarketData.mark_price("BTCUSDT")
-    {:ok,
-    %{
-      "estimatedSettlePrice" => "57311.26556567",
-      "indexPrice" => "57299.56815703",
-      "interestRate" => "0.00010000",
-      "lastFundingRate" => "0.00010000",
-      "markPrice" => "57325.76000000",
-      "nextFundingTime" => 1616342400000,
-      "symbol" => "BTCUSDT",
-      "time" => 1616341403005
-    }}
-
-
-    iex(2)> BinanceFutures.Rest.MarketData.mark_price()
-    {:ok,
-    [
+      iex(1)> BinanceFutures.Rest.MarketData.mark_price("BTCUSDT")
+      {:ok,
       %{
-        "estimatedSettlePrice" => "18.99792722",
-        "indexPrice" => "18.98676895",
+        "estimatedSettlePrice" => "57311.26556567",
+        "indexPrice" => "57299.56815703",
         "interestRate" => "0.00010000",
-        "lastFundingRate" => "0.00036359",
-        "markPrice" => "18.99830000",
+        "lastFundingRate" => "0.00010000",
+        "markPrice" => "57325.76000000",
         "nextFundingTime" => 1616342400000,
-        "symbol" => "SUSHIUSDT",
-        "time" => 1616341422000
-      },
-      %{
-          "estimatedSettlePrice" => "2.71120145",
-          "indexPrice" => "2.69011741",
+        "symbol" => "BTCUSDT",
+        "time" => 1616341403005
+      }}
+      iex(2)> BinanceFutures.Rest.MarketData.mark_price()
+      {:ok,
+      [
+        %{
+          "estimatedSettlePrice" => "18.99792722",
+          "indexPrice" => "18.98676895",
           "interestRate" => "0.00010000",
-          ...
+          "lastFundingRate" => "0.00036359",
+          "markPrice" => "18.99830000",
+          "nextFundingTime" => 1616342400000,
+          "symbol" => "SUSHIUSDT",
+          "time" => 1616341422000
         },
         %{
-          "estimatedSettlePrice" => "196.26837596",
-          "indexPrice" => "196.02526316",
+            "estimatedSettlePrice" => "2.71120145",
+            "indexPrice" => "2.69011741",
+            "interestRate" => "0.00010000",
+            ...
+          },
+          %{
+            "estimatedSettlePrice" => "196.26837596",
+            "indexPrice" => "196.02526316",
+            ...
+          },
+          %{"estimatedSettlePrice" => "3.96997638", ...},
+          %{...},
           ...
-        },
-        %{"estimatedSettlePrice" => "3.96997638", ...},
-        %{...},
-        ...
-      ]}
+        ]}
   """
   @spec mark_price(nil | binary) :: {:ok, map} | {:ok, [map]} | HTTPClient.error()
   def mark_price(symbol \\ nil),
