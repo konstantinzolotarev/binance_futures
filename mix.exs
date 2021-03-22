ce78c201099335712e0124ea016ce9b8c249ca27
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2021-03-22 21:46:19 +0200

    Up version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BinanceFutures.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/konstantinzolotarev/binance_futures"
 
   def project do
